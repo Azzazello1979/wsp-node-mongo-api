d1f62f961ee2e7c0d56478041106000c9565c1bf
Balint Haui <balint.haui@gmail.com>
2021-06-27 18:07:18 +0200

    users route fixed

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('./../controllers/usersController');
 
-router.get('/users', usersController.getAllUsers);
+router.get('/', usersController.getAllUsers);
 
 module.exports = router;
