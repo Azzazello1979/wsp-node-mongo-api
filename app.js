7f662726fce5d2ac823770ea238b7741ea129449
Balint Haui <balint.haui@gmail.com>
2021-06-27 14:00:36 +0200

    added middlewares

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 PORT = process.env.PORT || 7171 // <--- heroku will assign the port
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.status(200).send('Ok, running');
